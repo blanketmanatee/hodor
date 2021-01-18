4025c0b2cee1dbcdeec8c87762e720b6c9a9b0c0
blanketmanatee <ashleyl.welch@gmail.com>
2021-01-17 22:18:54 -0600

    this one seems to work

--- level_2/level_2.py
+++ level_2/level_2.py
@@ -2,15 +2,17 @@
 
 
 import requests
-from http import cookies
-
 url = 'http://158.69.76.135/level2.php'
 id = 1844
 time = 1024
-headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0"}
 
 for votes in range(time):
     passValues = {'id': '1844', 'holdthedoor': 'Submit', 'key': ''}
-    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
-    r = requests.get(url, headers=headers)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0"}
+
+    r = requests.get(url, headers = headers)
+    print(r.status_code)
     r = requests.post(url, data=passValues, headers=headers)
+    print(r.status_code)
+    print("done")
